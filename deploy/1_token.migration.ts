02ebb85884c3960d16f25d8363c2e429f1c3c4c4
Kyryl R <kyryl.ryabov@gmail.com>
2025-05-05 10:44:04 +0300

    Added deployment scripts

--- deploy/1_token.migration.ts
+++ deploy/1_token.migration.ts
@@ -1,9 +1,15 @@
 import { Deployer, Reporter } from "@solarity/hardhat-migrate";
 
-import { ERC20Mock__factory } from "@ethers-v6";
+import { SfUSD__factory } from "@ethers-v6";
+
+import { getConfig } from "@/deploy/config/config";
 
 export = async (deployer: Deployer) => {
-  const erc20 = await deployer.deploy(ERC20Mock__factory, ["Mock", "Mock", 18]);
+  const config = await getConfig();
 
-  await Reporter.reportContractsMD(["ERC20Mock", await erc20.getAddress()]);
+  const sfUSD = await deployer.deployERC1967Proxy(SfUSD__factory);
+
+  await sfUSD.__sfUSD_init(config.tokenName, config.tokenSymbol, config.rewardToken);
+
+  await Reporter.reportContractsMD(["SfUSD", await sfUSD.getAddress()]);
 };
